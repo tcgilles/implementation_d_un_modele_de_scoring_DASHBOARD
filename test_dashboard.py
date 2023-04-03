0083b27c2171e2cf9c01efe7314d5cb9af0f2ccb
Gilles Takam Cheno <chenogilles@gmail.com>
2023-04-03 13:31:46 +0200

    - Modification du fichier des clients (ouverture désormais à partir du lien github du fichier) - Dans la variable scoring, la sélection de l'id du client sera désormais fait avec un Dropdowm et pas un Input - Prise en compte des vrais scores des clients renvoyés par le modèle et disponibles dans la colonne SCOREdu fichier des clients

--- test_dashboard.py
+++ test_dashboard.py
@@ -5,11 +5,11 @@ from dashboard import plot_dist_feature_2, plot_bivariate_graph
 class Tests(unittest.TestCase):
 
     def test_credit_accepted(self):
-        result = update_credit_status(100002)
+        result = update_credit_status(100045)
         self.assertTupleEqual(result, ("Crédit accordé", {"color": "green", "textAlign": "center"}))
 
     def test_credit_declined(self):
-        result = update_credit_status(100003)
+        result = update_credit_status(100020)
         self.assertTupleEqual(result, ("Crédit refusé", {"color": "red", "textAlign": "center"}))
 
     def test_credit_invalid_id(self):
@@ -20,8 +20,8 @@ class Tests(unittest.TestCase):
         result = set_value_gauge(10)
         self.assertEqual(result, 1)
 
-    def test_value_gauge_id_100002(self):
-        result = set_value_gauge(100002)
+    def test_value_gauge_correct_id(self):
+        result = set_value_gauge(100045)
         self.assertLessEqual(result, 1)
         self.assertGreaterEqual(result, 0)
 
@@ -34,14 +34,14 @@ class Tests(unittest.TestCase):
         self.assertFalse(result[1])
     
     def test_incorrect_input_dist_feature_2(self):
-        result1 = plot_dist_feature_2(100002, "")
+        result1 = plot_dist_feature_2(100045, "")
         result2 = plot_dist_feature_2(100, "AMT_CREDIT")
         self.assertEqual(len(result1), 0)
         self.assertEqual(len(result2), 0)
     
     def test_incorrect_input_bivariate_graph(self):
-        result1 = plot_bivariate_graph(100002, "AMT_CREDIT", "", "Linear", "Linear")
-        result2 = plot_bivariate_graph(100002, "", "AMT_INCOME_TOTAL", "Linear", "Linear")
+        result1 = plot_bivariate_graph(100045, "AMT_CREDIT", "", "Linear", "Linear")
+        result2 = plot_bivariate_graph(100045, "", "AMT_INCOME_TOTAL", "Linear", "Linear")
         result3 = plot_bivariate_graph(100, "AMT_CREDIT", "AMT_INCOME_TOTAL", "Linear", "Linear")
         self.assertEqual(len(result1), 0)
         self.assertEqual(len(result2), 0)
